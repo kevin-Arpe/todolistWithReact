1a697213dd84ae80a79158b6566cef44ab271a06
kevinLee <arpeggiogio@gmail.com>
2020-03-27 17:24:56 +0900

    todoApp Template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TodoListTemplate form={<div>이 안에 폼 타입이 들어올 예정.</div>}>
+          <div>템플릿 완성</div>
+        </TodoListTemplate>
+      </div>
+    );
+  }
 }
 
 export default App;
